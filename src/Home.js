5bc34722614df9c27c82770a398c02b25895e58e
kerorogo23 <saddjk666@gmail.com>
2023-06-06 16:23:46 +0800

    加一個登出按鈕

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,27 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import './Home.css';
 
 function Home() {
+
+    const navigate = useNavigate(); // 建立 navigate 函式
+
+    const handleLogout = () => {
+        // 執行登出相關的邏輯，例如清除用戶資訊、驗證狀態等
+
+        // 轉換至登入頁面
+        navigate('/');
+    };
+
     return (
         <div className="app-container">
             <h1 className="app-heading">101書城後台</h1>
             <nav className="app-nav">
                 <ul className="app-nav-list">
+
+                    <li className="app-nav-item">
+                        <button onClick={handleLogout} className="app-nav-link">登出</button> {/* 修改：使用 button 元素處理登出事件 */}
+                    </li>
 
                     <li className="app-nav-item">
                         <Link to="/p-createProduct" className="app-nav-link">創建商品</Link>
